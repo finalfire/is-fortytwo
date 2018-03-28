f3f9f819783fbd75cd439037fa426b79221d2a89
finalfire <f.cauteruccio@gmail.com>
2018-03-28 12:13:32 +0200

    Added README and modified Cargo.toml

---
+++ README.md
@@ -0,0 +1,12 @@
+# is-fourtytwo
+
+A Rust library to check if a number is equal to fourtytwo.
+
+## Usage
+
+```rust
+fn main() {}
+```
+## Acknowledgments
+
+Freely inspired by [*is-thirteen*](https://github.com/jezen/is-thirteen) npm package.
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,8 @@
-/// `Is` is the core component of the library.
-/// It encapsulates an `i64` number.
-/// Note that it could be different from 42, although that's would be out of the library scope.
-pub struct Is {
-    number: i64
-}
-
-impl Is {
-    /// Create a new `Is`. To be executed by `Is::new(n)`.
-    pub fn new(n: i64) -> Is {
-        Is {number: n}
-    }
-
-    /// Check whether the number encapsulated by `Is` is fourtytwo or neither.
-    pub fn fourtytwo(self) -> bool {
-        self.number == 42
-    }
-
-    /// Adds `n` to the number encapsulated by `Is`.
-    /// Returns a new `Is`.
-    pub fn plus(self, n: i64) -> Is {
-        Is {number: self.number + n}
-    }
-
-    /// Substracts `n` to the number encapsulated by `Is`.
-    /// Returns a new `Is`.
-    pub fn minus(self, n: i64) -> Is {
-        Is {number: self.number - n}
-    }
-
-    /// Multiplies `n` to the number encapsulated by `Is`.
-    /// Returns a new `Is`.
-    pub fn times(self, n: i64) -> Is {
-        Is {number: self.number * n}
-    }
-}
-
+extern crate is_fourtytwo;
+use is_fourtytwo::Is;
 
 fn main() {
-    unimplemented!()
+    println!("{}", Is::new(42).fourtytwo());
 }
 
 #[test]
